cb0afb9605a7f986577903d6e340d1adf9e0db25
Colin D. <colinding10@gmail.com>
2025-05-19 13:54:31 -0400

    update result

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -49,6 +49,14 @@ interface Achievement {
 const achievements: Achievement[] = [
   // 2025
   {
+    event: "BYUCTF 2025",
+    placement: 17,
+    ctfPoints: 8634.0000,
+    ratingPoints: 28.858,
+    year: 2025,
+    eventUrl: "https://ctftime.org/event/2715",
+  }
+  {
     event: "San Diego CTF 2025",
     placement: 9,
     ctfPoints: 1625.0,
